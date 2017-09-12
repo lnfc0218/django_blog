07479d1ce14a526e47b3349434256e58b6b58ae1
lnfc0218 <pangh@n176-p109.eduroam.kth.se>
2017-09-12 11:55:47 +0200

    refactor for class-based views

--- comments/models.py
+++ comments/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.six import python_2_unicode_compatible
 
 
-# python_2_unicode_compatible 装饰器用于兼容 Python2
 @python_2_unicode_compatible
 class Comment(models.Model):
     name = models.CharField(max_length=100)
